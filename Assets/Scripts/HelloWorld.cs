ad914166c537dedafe7eefa51870946f2cdb0201
Jake Boxer <jake@github.com>
2014-09-20 23:35:48 -0700

    Print "Hello World" in console

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class HelloWorld : MonoBehaviour {
 
+	public List<string> list = new List<string>();
+	public float speed = 2f;
+
 	// Use this for initialization
 	void Start () {
-	
+		print (list[0] + " " + list[1]);
 	}
 	
 	// Update is called once per frame
