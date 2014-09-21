1af7576a26d9ddc48a97b4e24808f06333b2ab50
Jake Boxer <jake@github.com>
2014-09-21 02:11:25 -0700

    Make jetpack work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	public float speed = 10f;
 	public Vector2 maxVelocity = new Vector2(3, 5);
+	public bool standing;
+	public float jetSpeed = 15f;
+	public float airSpeedMultiplier = 0.3f;
 	
 	// Update is called once per frame
 	void Update () {
@@ -11,19 +14,32 @@ public class Player : MonoBehaviour {
 		var forceY = 0f;
 		
 		var absVelocityX = Mathf.Abs(rigidbody2D.velocity.x);
+		var absVelocityY = Mathf.Abs(rigidbody2D.velocity.y);
+
+		if (absVelocityY < 0.2f) {
+			standing = true;
+		} else {
+			standing = false;
+		}
 		
 		if (Input.GetKey("right")) {
 			if (absVelocityX < maxVelocity.x) {
-				forceX = speed;
+				forceX = standing ? speed : (speed * airSpeedMultiplier);
 			}
 
 			transform.localScale = new Vector3(1, 1, 1);
 		} else if (Input.GetKey("left")) {
 			if (absVelocityX < maxVelocity.x) {
-				forceX = -speed;
+				forceX = standing ? -speed : (-speed * airSpeedMultiplier);
 			}
 			
 			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		if (Input.GetKey("up")) {
+			if (absVelocityY < maxVelocity.y) {
+				forceY = jetSpeed;
+			}
 		}
 		
 		rigidbody2D.AddForce(new Vector2(forceX, forceY));
