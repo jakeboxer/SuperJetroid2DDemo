add6ab41092b98f1412d8b2c89ad811ce8cd1acb
Jake Boxer <jake@github.com>
2014-09-21 16:25:34 -0700

    Make switch pressable

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D target) {
+		animator.SetInteger("AnimState", 1);
+	}
+
+	void OnTriggerExit2D (Collider2D target) {
+		animator.SetInteger("AnimState", 2);
+	}
+}
