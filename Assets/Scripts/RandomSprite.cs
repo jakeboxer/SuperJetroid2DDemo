bf8e8d91057ef9b9d0a26106f9acf3203ad6fdc2
Jake Boxer <jake@github.com>
2014-09-21 15:13:10 -0700

    Allow manually setting crystal colors

--- Assets/Scripts/RandomSprite.cs
+++ Assets/Scripts/RandomSprite.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class RandomSprite : MonoBehaviour {
 	public Sprite[] sprites;
 	public string resourceName;
+	public int currentSpriteIndex = -1;
 
 	// Use this for initialization
 	void Start () {
 		if (resourceName != "") {
 			sprites = Resources.LoadAll<Sprite>(resourceName);
-			GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+
+			if (currentSpriteIndex == -1) {
+				currentSpriteIndex = Random.Range(0, sprites.Length);
+			} else if (currentSpriteIndex >= sprites.Length) {
+				currentSpriteIndex = sprites.Length - 1;
+			}
+
+			GetComponent<SpriteRenderer>().sprite = sprites[currentSpriteIndex];
 		}
 	}
 	
