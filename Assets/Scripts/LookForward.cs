e907d846409af2ca0fa388df17609af774ec62cf
Jake Boxer <jake@github.com>
2014-09-21 22:07:41 -0700

    Remove debug logging from LookForward script

--- Assets/Scripts/LookForward.cs
+++ Assets/Scripts/LookForward.cs
@@ -18,9 +18,7 @@ public class LookForward : MonoBehaviour {
 		Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
 
 		if (collision == needsCollision) {
-			this.transform.localScale = new Vector3((transform.localScale.x == 1) ? -1 : 1, 1, 1);
+			transform.localScale = new Vector3((transform.localScale.x == 1) ? -1 : 1, 1, 1);
 		}
-
-		Debug.Log("needsCollision: " + needsCollision);
 	}
 }
