df9e35932438defd559dd012e2c09d670d482fb4
Jake Boxer <jake@github.com>
2014-09-21 23:18:39 -0700

    Make AlienC show attack animation

---
+++ Assets/Scripts/AlienC.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienC : MonoBehaviour {
+	public float attackDelay = 3f;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+
+		if (attackDelay > 0) {
+			StartCoroutine(OnAttack());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetInteger("AnimState", 0);
+	}
+
+	IEnumerator OnAttack () {
+		yield return new WaitForSeconds(attackDelay);
+		Fire();
+		StartCoroutine(OnAttack());
+	}
+
+	void Fire () {
+		animator.SetInteger("AnimState", 1);
+	}
+}
