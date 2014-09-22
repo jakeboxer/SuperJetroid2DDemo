4a46aa7a93b287668adf3f942fa4c0682249c55f
Jake Boxer <jake@github.com>
2014-09-21 22:41:11 -0700

    Animate alien B attacking

---
+++ Assets/Scripts/AlienB.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienB : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D target) {
+		animator.SetInteger("AnimState", 1);
+	}
+}
