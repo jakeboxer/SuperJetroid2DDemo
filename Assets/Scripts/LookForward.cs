b1e3a8157e4fcd0460751e2deafab3ec37c2ea7e
Jake Boxer <jake@github.com>
2014-09-21 21:05:01 -0700

    Add LookForward script

---
+++ Assets/Scripts/LookForward.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookForward : MonoBehaviour {
+	public Transform sightStart, sightEnd;
+
+	private bool collision = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		collision = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Solid"));
+		Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
+	}
+}
