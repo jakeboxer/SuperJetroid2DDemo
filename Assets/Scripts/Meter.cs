56705cfa40ae3517d36094198328857ff9a6df60
Jake Boxer <jake@github.com>
2014-09-25 01:12:17 -0700

    Burn air while game runs

---
+++ Assets/Scripts/Meter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meter : MonoBehaviour {
+	public float air = 10f;
+	public float maxAir = 10f;
+	public float airBurnRate = 1f;
+
+	private Player player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!player) { return; }
+
+		if (air > 0) {
+			air -= Time.deltaTime * airBurnRate;
+		} else {
+			Explode script = player.GetComponent<Explode>();
+			script.OnExplode();
+		}
+	}
+}
