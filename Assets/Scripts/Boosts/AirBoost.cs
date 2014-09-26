50c31dcec1c0fc33b5fcc2616be03dacd06f968c
Jake Boxer <jake@github.com>
2014-09-25 23:47:26 -0700

    Add boost scripts
    * Add Meter.AddAir()
    * Make Collectable run OnBoost() on all boosts attached to the object

---
+++ Assets/Scripts/Boosts/AirBoost.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirBoost : Boost {
+	public float airAmount = 4f;
+	public Meter airMeter; 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void OnBoost () {
+		airMeter.AddAir(airAmount);
+	}
+}
