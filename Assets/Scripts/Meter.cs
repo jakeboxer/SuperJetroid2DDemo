4836e8258d2a2e2ae2fce55bdbb45e82fcb94b7d
Jake Boxer <jake@github.com>
2014-09-25 22:52:33 -0700

    Show air meter bg

--- Assets/Scripts/Meter.cs
+++ Assets/Scripts/Meter.cs
@@ -5,12 +5,35 @@ public class Meter : MonoBehaviour {
 	public float air = 10f;
 	public float maxAir = 10f;
 	public float airBurnRate = 1f;
+	public Texture2D bgTexture;
+	public Texture2D airBarTexture;
+	public int iconWidth = 32;
+	public Vector2 airOffset = new Vector2(10, 10);
 
 	private Player player;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindObjectOfType<Player>();
+	}
+
+	void OnGUI () {
+		float percent;
+
+		if (player) {
+			percent = Mathf.Clamp01(air / maxAir);
+		} else {
+			percent = 0f;
+		}
+
+		DrawMeter(airOffset.x, airOffset.y, airBarTexture, bgTexture, percent);
+	}
+
+	void DrawMeter (float x, float y, Texture2D foreground, Texture2D background, float percent) {
+		var bgWidth = background.width;
+		var bgHeight = background.height;
+
+		GUI.DrawTexture(new Rect(x, y, bgWidth, bgHeight), background);
 	}
 	
 	// Update is called once per frame
